52c2dcc6c7c8994b20c2ded919828c22a4c2b75b
Mustafa Asaad <mustafa.alrubiy@gmail.com>
2020-04-08 13:46:16 +0300

    Check redis-server

--- guardbot/utils/redisdb.py
+++ guardbot/utils/redisdb.py
@@ -1,4 +1,10 @@
-import redis
+import redis  
 
 """REDIS DATABASE UTILITY"""
-rdb = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+try:
+    rdb = redis.Redis(host='localhost', port=6379, decode_responses=True, socket_connect_timeout=1)
+    rdb.ping()
+except Exception as e:
+    print(e, '\nPlease make sure redis-server is installed and up running')
+    exit()
